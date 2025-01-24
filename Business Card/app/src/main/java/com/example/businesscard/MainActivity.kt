78b3089e9c5464f8d4208e0a76d3cb950bb45825
Thisisyusub <kanan.yusub@gmail.com>
2025-01-24 17:42:30 +0400

    dice roller added

--- Business Card/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ Business Card/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -171,3 +171,34 @@ fun PreviewPersonInfo() {
         )
     }
 }
+
+
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    override fun turnOn() {
+        super.turnOn()
+    }
+}
+
+
+open class SmartDevice(val name: String, val category: String) {
+    var deviceStatus = "online"
+
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+
+    open fun turnOn() {}
+}

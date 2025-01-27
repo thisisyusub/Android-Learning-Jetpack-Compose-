daf3d4e708f5c6b0efc886c55930c95a01ecd1f2
Thisisyusub <kanan.yusub@gmail.com>
2025-01-27 14:31:37 +0400

    affirmations app added

---
+++ Affirmations/app/src/main/java/com/example/affirmations/Affirmation.kt
@@ -0,0 +1,8 @@
+package com.example.affirmations
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Affirmation(
+    @StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int
+)
